4a8aeabf97bda89bdc61bb8867cb175b11b83edf
mcaguiar234 <109763544+mcaguiar234@users.noreply.github.com>
2025-05-28 21:58:01 -0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,7 +3,7 @@ import joblib
 # Datos de prueba
 elementos = [[ 0, -0.52977177,  0.52644436, -0.64073999, -0.19534317,  1]]
 # Cargar el modelo
-modelo = joblib.load("modelo.pkl")
+modelo = joblib.load("mlp_model.pkl")
 # Hacer la predicción
 prediccion = modelo. predict(elementos)
 print("Prediccion:", prediccion[0])
