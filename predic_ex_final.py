882bc273cf3d68a9fd79a2ad2e382a6eaad28beb
mcaguiar234 <109763544+mcaguiar234@users.noreply.github.com>
2025-05-31 10:08:47 -0500

    Update predic_ex_final.py

--- predic_ex_final.py
+++ predic_ex_final.py
@@ -4,6 +4,8 @@ import joblib
 elementos = [[ 149, 133,  133, 93, 68,  162, 140, 0]]
 # Cargar el modelo
 modelo = joblib.load("random_forest_model.pkl")
+label_encoder = joblib.load("label_encoder.pkl") 
+
 # Hacer la predicción
 prediccion = modelo. predict(elementos)
 print("Prediccion:", prediccion[0])
