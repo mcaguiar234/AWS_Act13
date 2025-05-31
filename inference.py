e6602a1a17aa2d998d3122cf9f0c8f0edfd0d028
mcaguiar234 <109763544+mcaguiar234@users.noreply.github.com>
2025-05-31 10:01:35 -0500

    Delete inference.py

--- inference.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-# Datos de prueba
-elementos = [[ 0, -0.52977177,  0.52644436, -0.64073999, -0.19534317,  1]]
-# Cargar el modelo
-modelo = joblib.load("mlp_model.pkl")
-# Hacer la predicción
-prediccion = modelo. predict(elementos)
-print("Prediccion:", prediccion[0])
