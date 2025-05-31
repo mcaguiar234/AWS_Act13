e7660ac424cd5f2c4561b8724fd7d03e60f1fb11
mcaguiar234 <109763544+mcaguiar234@users.noreply.github.com>
2025-05-31 11:01:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,26 +18,22 @@ def formulario(request: Request):
 @app.post("/", response_class=HTMLResponse)
 def predecir_desde_formulario(
     request: Request,
-    cantidad_imagenes: int = Form(...),
     ancho_promedio: int = Form(...),
     alto_promedio: int = Form(...),
     ancho_minimo: int = Form(...),
     alto_minimo: int = Form(...),
     ancho_maximo: int = Form(...),
-    alto_maximo: int = Form(...),
-    archivos_corruptos: int = Form(...)
+    alto_maximo: int = Form(...)
 ):
-    # Crear DataFrame con nombres que espera el modelo
     df = pd.DataFrame([{
-        "image_count": cantidad_imagenes,
         "avg_width": ancho_promedio,
         "avg_height": alto_promedio,
         "min_width": ancho_minimo,
         "min_height": alto_minimo,
         "max_width": ancho_maximo,
-        "max_height": alto_maximo,
-        "corrupt_files": archivos_corruptos
+        "max_height": alto_maximo
     }])
+
     pred = modelo.predict(df)
     clase = label_encoder.inverse_transform(pred)[0]
     return templates.TemplateResponse("form.html", {"request": request, "prediccion": clase})
